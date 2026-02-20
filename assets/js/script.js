99a9acddee009369d1042559a8f9491b952d8121
majnioui <majnioui@gmail.com>
2026-02-20 22:46:08 +0000

    remove giscus comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,6 @@ function setTheme(theme) {
 		modeSwitcher.classList.toggle('active', theme === 'dark');
 	}
 	localStorage.setItem('theme', theme === 'dark' ? 'dark' : '');
-	updateGiscusTheme(theme);
-}
-
-function updateGiscusTheme(theme) {
-	const giscusFrame = document.querySelector('iframe.giscus-frame');
-	if (giscusFrame) {
-		giscusFrame.contentWindow.postMessage({
-			giscus: {
-				setConfig: { theme }
-			}
-		}, 'https://giscus.app');
-	}
 }
 
 function toggleNightMode() {
@@ -27,14 +15,4 @@ function toggleNightMode() {
 document.addEventListener('DOMContentLoaded', () => {
 	const theme = localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
 	setTheme(theme);
-});
-
-window.addEventListener('message', event => {
-	if (event.origin !== 'https://giscus.app') return;
-	if (!(typeof event.data === 'object' && event.data.giscus)) return;
-
-	if (event.data.giscus.resizeHeight) {
-		const theme = localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
-		updateGiscusTheme(theme);
-	}
 });
